5d2673725e7accd872bce90fa9b5271ae0b670f0
L3ofspades <jonathanfederico13@gmail.com>
2025-04-13 20:26:10 -0700

    Updated sketch.js
    Added Comments

--- sketch.js
+++ sketch.js
@@ -19,5 +19,10 @@ function draw() {
   x = x + speed;
 }
 
+// Practicing java script and p5.js
+// This is a simple sketch that draws a circle that moves back and forth across the canvas.
+// The circle bounces off the edges of the canvas.
+// The speed of the circle is controlled by the variable 'speed'.
+
 
 
